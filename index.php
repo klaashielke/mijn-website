5bb1ffb2b675b50549982ad08daebf82eff37db4
Klaas Hielke <klaashielke.dijkgraaf@gxsoftware.com>
2023-12-25 20:14:32 +0100

    improve grid sizing and add scroll snap behavior

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-    <meta charset="UTF-8"> <!-- TODO: uitzoeken welke hier moet -->
+    <meta charset="UTF-8"> 
+    <meta name="description" content="Persoonlijke website van Klaas Hielke">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klaas Hielke</title>
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- TODO: Other meta tags, stylesheets, or scripts can be included here -->
+    <!-- TODO: favicon -->
 </head>
 <body>
     <?php include 'includes/header.php'; ?>
 
-    <div class="container">
-        <div class="item">
-            <h1>Welkom op mijn website</h1>
+    <div class="grid-container">
+        <div class="grid-item">
+            <h1>Welkom</h1>
         </div>
-        <div class="item">
-            <p>Het is hier nog vrij leeg.</p>
-        </div>
-        <div class="item">
-            <h1 style="color:red">Hallo Boaz</h1>
-        </div>
-        <div class="item">
-            <h1 style="color:blue">Sarah Kerst</h1>
-        </div>
-        <div class="item">
-            <img style="background-color: white" src="assets/icons/linkedin.svg" alt="LinkedIn icon" />
+        <div class="grid-item">
+            <a href="https://www.linkedin.com/in/klaas-hielke-dijkgraaf-2a736479/">
+                <img style="background-color: white" src="assets/icons/linkedin.svg" alt="LinkedIn icon" />
+            </a>
         </div>
     </div>
 
