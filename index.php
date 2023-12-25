42ff6dfedfdec084a310d4a1ab1a5e2048737d2c
Klaas Hielke <klaashielke.dijkgraaf@gxsoftware.com>
2023-12-25 15:42:31 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8"> <!-- TODO: uitzoeken welke hier moet -->
+    <title>Klaas Hielke</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- TODO: Other meta tags, stylesheets, or scripts can be included here -->
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+
+    <div class="container">
+        <div class="item">
+            <h1>Welkom op mijn website</h1>
+        </div>
+        <div class="item">
+            <p>Het is hier nog vrij leeg.</p>
+        </div>
+        <div class="item">
+            <h1 style="color:red">Hallo Boaz</h1>
+        </div>
+        <div class="item">
+            <h1 style="color:blue">Sarah Kerst</h1>
+        </div>
+        <div class="item">
+            <img style="background-color: white" src="assets/icons/linkedin.svg" alt="LinkedIn icon" />
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
