9ca56b6b71d66674a12cc55f52ee88f9d11999ef
Klaas Hielke <klaashielke.dijkgraaf@gxsoftware.com>
2023-12-25 23:43:05 +0100

    add icons with zoom

--- index.php
+++ index.php
@@ -20,13 +20,18 @@
         <div class="grid-item">
             <div class="card">
                 <h2>Vind mij op</h2>
-                <a href="https://www.linkedin.com/in/klaas-hielke-dijkgraaf-2a736479/">
-                    <img style="background-color: white" src="assets/icons/linkedin.svg" alt="LinkedIn icon" />
-                </a>
+                <div class="link-buttons">
+                    <a href="https://www.linkedin.com/in/klaas-hielke-dijkgraaf-2a736479/">
+                        <img class="icon-link" src="assets/icons/linkedin.svg" alt="LinkedIn icon" />
+                    </a>
+                    <a href="https://github.com/klaashielke">
+                        <img class="icon-link" src="assets/icons/github.svg" alt="GitHub icon"/>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 
-    <?php include 'includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?> 
 </body>
 </html>
